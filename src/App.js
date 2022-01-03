24124e4191dca9b022d55d1b226500c1c607183d
3minhaz <3minhaz@gmail.com>
2022-01-03 17:17:33 +0600

    all programs added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import Dashboard from './components_minhaz/Dashboard/Dashboard/Dashboard';
+import AddPackeges from './components_minhaz/AddPackeges/AddPackeges';
+import ManagePrograms from './components_minhaz/ManagePrograms/ManagePrograms';
+import AllPrograms from './components_minhaz/AllPrograms/AllPrograms';
 import {
   BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
-import AddPackeges from './components_minhaz/AddPackeges/AddPackeges';
-import ManagePrograms from './components_minhaz/ManagePrograms/ManagePrograms';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
+          <Route path="/allPrograms" element={<AllPrograms />} />
           <Route path="dashboard" element={<Dashboard />} >
             <Route path="addPrograms" element={<AddPackeges></AddPackeges>}></Route>
             <Route path="managePrograms" element={<ManagePrograms></ManagePrograms>}></Route>
---
+++ src/components_minhaz/AllPrograms/AllPrograms.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllPrograms = () => {
+    return (
+        <div>
+            <h2>this is all programs</h2>
+        </div>
+    );
+};
+
+export default AllPrograms;
